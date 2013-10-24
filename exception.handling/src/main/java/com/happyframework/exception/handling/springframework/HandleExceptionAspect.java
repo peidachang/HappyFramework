719bd858a28355b7f0020db1d56d2dfedcc2860d
段光伟 <shijiucha@qq.com>
2013-10-24 13:35:52 +0800

    提交修改！

--- exception.handling/src/main/java/com/happyframework/exception/handling/springframework/HandleExceptionAspect.java
+++ exception.handling/src/main/java/com/happyframework/exception/handling/springframework/HandleExceptionAspect.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
